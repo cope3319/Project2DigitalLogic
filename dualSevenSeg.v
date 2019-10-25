1549a0129ab6bdef700f8b654287dd8db9de0b4b
cope3319 <peskin.12@gmail.com>
2019-10-25 17:12:33 -0600

    aye

--- dualSevenSeg.v
+++ dualSevenSeg.v
@@ -31,16 +31,16 @@ end
 always @*
 begin
 casex (data2[3:0])
-4'b0000 : display1 = 8'b11000000; //0
-4'b0001 : display1 = 8'b11111001; //1
-4'b0010 : display1 = 8'b10100100; //2
-4'b0011 : display1 = 8'b10110000; //3
-4'b0100 : display1 = 8'b10011001; //4
-4'b0101 : display1 = 8'b10010010; //5
-4'b0110 : display1 = 8'b10000010; //6
-4'b0111 : display1 = 8'b11111000; //7
-4'b1000 : display1 = 8'b10000000; //8
-4'b1001 : display1 = 8'b10010000; //9
+4'b0000 : display2 = 8'b11000000; //0
+4'b0001 : display2 = 8'b11111001; //1
+4'b0010 : display2 = 8'b10100100; //2
+4'b0011 : display2 = 8'b10110000; //3
+4'b0100 : display2 = 8'b10011001; //4
+4'b0101 : display2 = 8'b10010010; //5
+4'b0110 : display2 = 8'b10000010; //6
+4'b0111 : display2 = 8'b11111000; //7
+4'b1000 : display2 = 8'b10000000; //8
+4'b1001 : display2 = 8'b10010000; //9
 endcase
 end
 endmodule
--- monthDayCalc.v
+++ monthDayCalc.v
@@ -1,4 +1,11 @@
-module monthDayCalc(data,month,day)
-
+module monthDayCalc(data,month,day1,day2)
+//input number of the day of the year, outputs the month and day
+// day1 is leftmost 7-seg (Overflow bit) , day2 is rightmost 7-seg
+input [6:0] data;
+output [4:0] month;
+output [7:0] day1;
+output [7:0] day2;
+reg [7:0] day1;
+reg [7:0] day2;
 
 endmodule
