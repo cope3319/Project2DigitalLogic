2fd5696e81d374e7b872392f5738b6ab9e9aaf91
cope3319 <peskin.12@gmail.com>
2019-11-04 07:58:47 -0700

    whatishappeninglol

--- tb_project2.v
+++ tb_project2.v
@@ -1,9 +1,9 @@
 /* USE:
- ~from directory project2digitallogic
+ ~from directory project2digitallogic/
  ~iverilog tb_project2.v top.v segToDec.v monthDayCalc.v dualSevenSeg.v clock_divider.v counted.v
  ~vvp a.out > out.txt
  */
-`timescale 1 ns / 100 ps
+`timescale 1 s / 100 ms
 
 module tb();
 
@@ -17,24 +17,13 @@ wire [7:0] HEX4; //Digit 1
 wire [7:0] HEX5; //Digit 2
 wire [1:0] LEDR;
 
-wire [3:0]day1; //This is the HEX1 number representation
-wire [3:0]day2; //This is the HEX0 number representation
-wire [3:0]right;
-wire [3:0]left; 
-
-/*
-//get decimal values for each to print alongside
 reg [3:0] day1digit;
-reg [3:0] day2;
+reg [3:0] day2digit;
 reg [3:0] monthDigit;
 reg [3:0] unusedMonthDig;
 reg [3:0] digit1digit;
 reg [3:0] digit2digit;
 
-segToDec(.seg1(HEX1),.seg2(HEX0),.dig1(day1digit),.dig2(day2));
-segToDec(.seg1(HEX4),.seg2(HEX5),,dig1(digit1digit),.dig2(digit2digit));
-segToDec(.seg1(HEX3),.seg2(HEX2),.dig1(unusedMonthDig),.dig2(monthDigit));
-*/
 
 top DUT(
   .SW(SW),
@@ -45,11 +34,7 @@ top DUT(
   .HEX3(HEX3),
   .HEX4(HEX4),
   .HEX5(HEX5),
-  .LEDR(LEDR),
-  .day1(day1),
-  .day2(day2),
-  .right(right),
-  .left(left)
+  .LEDR(LEDR)
 );
 
 initial
@@ -58,32 +43,107 @@ begin
 	$dumpvars;
     $display($time, " << Starting Simulation >>");
     SW[9:8] = 2'b00;
+    KEY[1:0] = 2'b00;
+    $display($time, " SW[9] low: %b, no buttons KEY[1:0]= %b",SW[9],KEY[1:0]);
+    //KEY[1:0] = 2'b10;
+    #1
+    $display($time, " Pressing reset to begin, KEY[0] = %b, HEX3 Cleared: %b",KEY[0],HEX3[7:0]);
     KEY[1:0] = 2'b11;
-    $display($time, " SW[9] low: %b, no buttons KEY[1:0]= = %b",SW[9],KEY[1:0]);
-    $dumpvars;
-    KEY[0] = 1'b0;
-    $display($time, " Pressing reset to begin, KEY[0] = %b, HEX2 Cleared: %b",KEY[0],HEX2[7:0]);
-    #1000
-    $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
-    $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
-    $display($time, " Converted to digits: %b %b - %b - %b %b",
-                    digit2digit[3:0],digit1digit[3:0],monthDigit[3:0],day1[3:0],day2[3:0]);
-    /*repeat(100) begin
-        if($time == 1000) KEY[0] = 1'b1; //unpressing button after first second
+    #1
+    KEY[1:0] = 2'b10;
+    repeat(10) begin
+        #1
         $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
         $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
-        $display($time, " Converted to digits: %b %b - %b %b - %b %b",
-                          digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0],monthDigit[3:0],day1[3:0],day2[3:0]);
-        #1000
-    end*/
-    $display($time, " Ending Simulation, Connor Peskin");
+        $display($time, " Converted to digits: %d %d - %d %d - %d %d",
+                   digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+    end
+    
+    $display($time, " Ending Simulation, by Connor Peskin");
 	$finish;
 end
-  
+
 initial
 begin
-      $monitor($time, " Buttons(KEY): %b, Day2(HEX0): %b, Day1(HEX1): %b, Month(HEX2): %b, Digit1(HEX4): %b, Digit2(HEX5): %b",
-                              KEY[1:0],      HEX0[7:0],      HEX1[7:0],       HEX2[7:0],        HEX4[7:0],        HEX5[7:0]);
+      $monitor($time, " Buttons(KEY): %b, Day2(HEX0): %b, Day1(HEX1): %b, UnusedMonth(HEX3): %b, Month(HEX2): %b, Digit1(HEX4): %b, Digit2(HEX5): %b",
+                              KEY[1:0],      HEX0[7:0],      HEX1[7:0],               HEX3[7:0], HEX2[7:0],        HEX4[7:0],        HEX5[7:0]);
+end
+
+
+always @* 
+begin
+    casex(HEX1[7:0])
+        8'b11000000 : day1digit = 4'b0000; //0
+        8'b11111001 : day1digit = 4'b0001; //1
+        8'b10100100 : day1digit = 4'b0010; //2
+        8'b10110000 : day1digit = 4'b0011; //3
+        8'b10011001 : day1digit = 4'b0100; //4
+        8'b10010010 : day1digit = 4'b0101; //5
+        8'b10000010 : day1digit = 4'b0110; //6
+        8'b11111000 : day1digit = 4'b0111; //7
+        8'b10000000 : day1digit = 4'b1000; //8
+        8'b10010000 : day1digit = 4'b1001; //9
+    endcase
+    casex(HEX0[7:0])
+        8'b11000000 : day2digit = 4'b0000; //0
+        8'b11111001 : day2digit = 4'b0001; //1
+        8'b10100100 : day2digit = 4'b0010; //2
+        8'b10110000 : day2digit = 4'b0011; //3
+        8'b10011001 : day2digit = 4'b0100; //4
+        8'b10010010 : day2digit = 4'b0101; //5
+        8'b10000010 : day2digit = 4'b0110; //6
+        8'b11111000 : day2digit = 4'b0111; //7
+        8'b10000000 : day2digit = 4'b1000; //8
+        8'b10010000 : day2digit = 4'b1001; //9
+    endcase
+    casex(HEX2[7:0])
+        8'b11000000 : monthDigit = 4'b0000; //0
+        8'b11111001 : monthDigit = 4'b0001; //1
+        8'b10100100 : monthDigit = 4'b0010; //2
+        8'b10110000 : monthDigit = 4'b0011; //3
+        8'b10011001 : monthDigit = 4'b0100; //4
+        8'b10010010 : monthDigit = 4'b0101; //5
+        8'b10000010 : monthDigit = 4'b0110; //6
+        8'b11111000 : monthDigit = 4'b0111; //7
+        8'b10000000 : monthDigit = 4'b1000; //8
+        8'b10010000 : monthDigit = 4'b1001; //9
+    endcase
+    casex(HEX3[7:0])
+        8'b11000000 : unusedMonthDig = 4'b0000; //0
+        8'b11111001 : unusedMonthDig = 4'b0001; //1
+        8'b10100100 : unusedMonthDig = 4'b0010; //2
+        8'b10110000 : unusedMonthDig = 4'b0011; //3
+        8'b10011001 : unusedMonthDig = 4'b0100; //4
+        8'b10010010 : unusedMonthDig = 4'b0101; //5
+        8'b10000010 : unusedMonthDig = 4'b0110; //6
+        8'b11111000 : unusedMonthDig = 4'b0111; //7
+        8'b10000000 : unusedMonthDig = 4'b1000; //8
+        8'b10010000 : unusedMonthDig = 4'b1001; //9
+    endcase
+    casex(HEX4[7:0])
+        8'b11000000 : digit1digit = 4'b0000; //0
+        8'b11111001 : digit1digit = 4'b0001; //1
+        8'b10100100 : digit1digit = 4'b0010; //2
+        8'b10110000 : digit1digit = 4'b0011; //3
+        8'b10011001 : digit1digit = 4'b0100; //4
+        8'b10010010 : digit1digit = 4'b0101; //5
+        8'b10000010 : digit1digit = 4'b0110; //6
+        8'b11111000 : digit1digit = 4'b0111; //7
+        8'b10000000 : digit1digit = 4'b1000; //8
+        8'b10010000 : digit1digit = 4'b1001; //9
+    endcase
+    casex(HEX5[7:0])
+        8'b11000000 : digit2digit = 4'b0000; //0
+        8'b11111001 : digit2digit = 4'b0001; //1
+        8'b10100100 : digit2digit = 4'b0010; //2
+        8'b10110000 : digit2digit = 4'b0011; //3
+        8'b10011001 : digit2digit = 4'b0100; //4
+        8'b10010010 : digit2digit = 4'b0101; //5
+        8'b10000010 : digit2digit = 4'b0110; //6
+        8'b11111000 : digit2digit = 4'b0111; //7
+        8'b10000000 : digit2digit = 4'b1000; //8
+        8'b10010000 : digit2digit = 4'b1001; //9
+    endcase
 end
 
 
