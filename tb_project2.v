6807b9ae29086848a7ea53f5b874ce6bffc6cf83
cope3319 <cope3319@colorado.edu>
2019-11-05 13:17:01 -0700

    workingWithAllExamples

--- tb_project2.v
+++ tb_project2.v
@@ -3,7 +3,7 @@
  ~iverilog tb_project2.v top.v monthDayCalc.v dualSevenSeg.v clock_divider.v counted.v
  ~vvp a.out > out.txt
  */
-`timescale 1 s / 100 ns
+`timescale 1 ms / 100 ps
 
 module tb();
 
@@ -25,9 +25,9 @@ reg [3:0] unusedMonthDig;
 reg [3:0] digit1digit;
 reg [3:0] digit2digit;
 
-always #1 ADC_CLK_10 = ~ADC_CLK_10;
+always #1 ADC_CLK_10 = ~ADC_CLK_10; //will be a 500Hz clk
 
-top #(1,2) DUT(
+top #(250,50) DUT(
   .KEY(KEY),
   .HEX0(HEX0),
   .HEX1(HEX1),
@@ -45,17 +45,56 @@ begin
     $dumpfile("out.vcd");
 	$dumpvars;
     $display($time, " << Starting Simulation >>");
-    ADC_CLK_10 = 1'b0;
+    ADC_CLK_10 = 1'b1;
     SW[9:8] = 2'b00;
     KEY[1:0] = 2'b11;
     $display($time, " SW[9] low: %b, no buttons KEY[1:0]= %b",SW[9],KEY[1:0]);
-    #3
+    #1000 
     KEY[1:0] = 2'b10;
-    #1
+    #250
     $display($time, " Pressing reset to begin, KEY[0] = %b, HEX3 Cleared: %b",KEY[0],HEX3[7:0]);
-    repeat(100) begin
-        if($time<5) KEY[1:0] = 2'b11;
-        #1
+    $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
+    $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
+    $display($time," Converted to digits: %d %d - %d %d - %d %d",
+                digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+    repeat(98) begin
+        #1000
+        $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
+        $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
+        $display($time," Converted to digits: %d %d - %d %d - %d %d",
+                   digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+        
+    end
+    SW[9] = 1'b1;
+    $display($time, " Now turning on leap year-> SW[9]=%b",SW[9]);
+    KEY[1:0] = 2'b10;
+    #250
+    $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
+    $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
+    $display($time," Converted to digits: %d %d - %d %d - %d %d",
+                digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+    KEY[1:0] = 2'b11;
+    repeat(98) begin
+        #1000
+        $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
+        $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
+        $display($time," Converted to digits: %d %d - %d %d - %d %d",
+                   digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+    end
+    KEY[0] = 1'b0;
+    #250
+    KEY[0] = 1'b1;
+    #250 
+    KEY[1:0] = 2'b00;
+    $display($time, " Now pressing KEY[1]: %b",KEY[1]);
+    #1000
+    $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
+    $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
+    $display($time," Converted to digits: %d %d - %d %d - %d %d",
+                digit2digit[3:0],digit1digit[3:0],unusedMonthDig[3:0], monthDigit[3:0],day1digit[3:0],day2digit[3:0]);
+    KEY[0] = 1'b1;
+    repeat(98) begin
+        #500
         $display($time, " Left Digit: HEX5 = %b, Right Digit: HEX4 = %b", HEX5[7:0], HEX4[7:0]);
         $display($time, " Left Day: HEX1 = %b, Right Day: HEX0 = %b, Month: HEX2 = %b",HEX1[7:0],HEX0[7:0],HEX2[7:0]);
         $display($time," Converted to digits: %d %d - %d %d - %d %d",
@@ -68,8 +107,8 @@ end
 
 initial
 begin
-      $monitor($time, " Buttons(KEY): %b, Day2(HEX0): %b, Day1(HEX1): %b, UnusedMonth(HEX3): %b, Month(HEX2): %b, Digit1(HEX4): %b, Digit2(HEX5): %b, Clk: %b",
-                              KEY[1:0],      HEX0[7:0],      HEX1[7:0],               HEX3[7:0], HEX2[7:0],        HEX4[7:0],        HEX5[7:0], ADC_CLK_10);
+      $monitor($time, " Buttons(KEY): %b, Day2(HEX0): %b, Day1(HEX1): %b, UnusedMonth(HEX3): %b, Month(HEX2): %b, Digit1(HEX4): %b, Digit2(HEX5): %b",
+                              KEY[1:0],      HEX0[7:0],      HEX1[7:0],               HEX3[7:0], HEX2[7:0],        HEX4[7:0],        HEX5[7:0]);
 end
 
 
